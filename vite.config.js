fc3ddb3d0db4dc1e3f7bf4b6cb6949d123cbc5d6
Anand Shende <anand.shende@videocx.io>
2026-04-14 13:40:07 +0530

    login fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        admin: resolve(__dirname, 'admin.html')
+        admin: resolve(__dirname, 'admin.html'),
+        dashboard: resolve(__dirname, 'dashboard.html')
       }
     }
   }
